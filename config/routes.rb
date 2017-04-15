fa2255c614174bddca4c71f2a5bc07abbca617a2
Peter Kananen <peter@gaslight.co>
2017-04-15 00:22:48 -0400

    first route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/flow_metrics" => 'flow_metrics#index'
+  get "/" => 'flow_metrics#index'
 end
